cc82448f145fc7df079d0fdba12645627e0fe6e7
Danilo Lima <danilo.lima@dmcgroup.com.br>
2023-01-16 08:11:15 -0300

    Adiciona MLDUal

--- OmniaScript/config.py
+++ OmniaScript/config.py
@@ -8,5 +8,7 @@ CONFIG_TESTS = {
     'TheraLaseSurgery_6-INSP-493_rev03.Omnia':
         ("TheraLaseSurgery", range(1,10)),
     'Therapy.Omnia': 
-        ("Therapy", range(1,8))
+        ("Therapy", range(1,8)),
+    'MediLaserDual_6-INSP-495_rev.04' :
+        ("MedilaserDual", range(1,15)),
 }
