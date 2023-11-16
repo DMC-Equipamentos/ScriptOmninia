3ed71825f91527e528cd4afc7339cb01604be86e
danilo <danilo.lima@dmcgroup.com.br>
2023-11-16 14:12:42 -0300

    remove warning quando é utilizado um arquivo antigo

--- OmniaScript/filesread.py
+++ OmniaScript/filesread.py
@@ -42,6 +42,10 @@ def detectVersionAndUniform(data):
     
     if 'DUT Serial' in data.columns: # arquivo novo!
         
+        # Modelo do equipamento
+        data['Model Number'] = data['DUT Model']
+        del data['DUT Model']
+        
         # modifica coluna com o número de série
         data['Serial Number'] = data['DUT Serial']
         del data['DUT Serial']
@@ -54,7 +58,6 @@ def detectVersionAndUniform(data):
         data['Pass/Fail'] = data['Status'].apply(lambda st: st.capitalize())
         del data['Status']
         
-    print(data)
     return data
 
 
@@ -63,21 +66,27 @@ def readValidFile(path):
     extension = os.path.splitext(path)[-1]
     
     # HEADERS do arquivo (não deve ser alterado)
-    # header = ["Date", "Time", "Omnia File Name", "PC File Name", 
-    #         "File", "Step", "Test Type", "Pass/Fail", 
-    #         "Test Result", "Meter 1", "Meter 2", "Meter 3", 
-    #         "Timer", "Meter 4", "Meter 5", "Model Number", 
-    #         "Serial Number", "Probe", "Measuring Device", 
-    #         "Test Model #", "Test Serial #", "Calibration Due Date", 
-    #         "Operator ID", "Step 2"]
+    header_xls = ["Date", "Time", "Omnia File Name", "PC File Name", 
+            "File", "Step", "Test Type", "Pass/Fail", 
+            "Test Result", "Meter 1", "Meter 2", "Meter 3", 
+            "Timer", "Meter 4", "Meter 5", "Model Number", 
+            "Serial Number", "Probe", "Measuring Device", 
+            "Test Model #", "Test Serial #", "Calibration Due Date", 
+            "Operator ID", "Step 2"]
+
+    # Headers csv (arquivo novo usa headers diferentes)
+    headers_csv = [
+        'Test ID','Test File Name','Instrument','Serial #','DUT Model',
+        'DUT Serial','Date & Time','Step','Type',
+        'Status','Meter 1','Meter 2','Meter 3','Meter 4','Meter 5','Meter 6'
+    ]
 
     # lê arquivo csv (não utiliza o HEADER do arquivo, 
-    # que vem faltando a última coluna)
-
+    # que vem faltando a última coluna) quando é xls
     if extension in ['.csv', '.xls']:
-        data = pd.read_csv(path, sep=None, engine='python', index_col=False)
-                        # names = header, 
-                        # header=None, skiprows=1)
+        headers = headers_csv if extension == '.csv' else header_xls
+        data = pd.read_csv(path, sep=None, engine="python", index_col=False,
+                        names = headers, header=None, skiprows=1)
     else :
         data = pd.read_excel(path, sep = None)
     
